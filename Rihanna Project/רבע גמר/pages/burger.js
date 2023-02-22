92bafd95223c409073941ea94b2eb5a94bc6f65a
adiharazi <126096840+adiharazi@users.noreply.github.com>
2023-02-22 23:44:59 +0200

    Add files via upload

---
+++ Rihanna Project/רבע גמר/pages/burger.js
@@ -0,0 +1,18 @@
+function burger_init() {
+  // הכפתור של הבורגר צריך את האיי די הזה
+  // console.log("burger")
+  let burger_btn = document.querySelector("#burger_btn");
+
+  // התפריט שייעלם ויחזור צריך
+  // את האיי די הזהo
+  var id_nav_show_hide = document.querySelector("#id_nav_show_hide");
+  console.log(id_nav_show_hide)
+  burger_btn.addEventListener("click", function () {
+    id_nav_show_hide.style.display =
+      id_nav_show_hide.style.display != "flex" ? "flex" : "none";
+  });
+}
+
+burger_init();
+
+
